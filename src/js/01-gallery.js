70e0446510123a0005401aab00747808ac0a1c18
Mixasikus <mixasiikus@gmail.com>
2022-06-08 17:17:28 +0300

    закончил 1 задание

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,26 +1,13 @@
-import SimpleLightbox from 'simplelightbox';
 import { galleryItems } from './gallery-items.js';
 
 const galleryListEl = document.querySelector('.gallery');
 const galleryItemEl = galleryItems
   .map(
-    ({
-      preview,
-      original,
-      description,
-    }) => `<a class="gallery__item" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      alt="${description}"
-      title="${description}" 
-    />
-  </a>`
+    ({ preview, original, description }) =>
+      `<a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/></a>`
   )
   .join('');
 
 galleryListEl.insertAdjacentHTML('beforeend', galleryItemEl);
 
-// const simple = SimpleLightbox('.gallery a');
-
-const lightbox = new SimpleLightbox('.gallery a');
+new SimpleLightbox('.gallery a');
